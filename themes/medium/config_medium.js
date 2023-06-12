93058b1cb521ffe30b17ecbf8def05669c4d96b5
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-06-12 18:49:52 +0800

    Update config_medium.js

--- themes/medium/config_medium.js
+++ themes/medium/config_medium.js
@@ -4,7 +4,7 @@ const CONFIG_MEDIUM = {
   RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MEDIUM_RIGHT_DARK || false, // 右侧面板深色模式
 
   POST_LIST_COVER: false, // 文章列表显示图片封面
-  POST_LIST_PREVIEW: false, // 列表显示文章预览
+  POST_LIST_PREVIEW: true, // 列表显示文章预览
   POST_LIST_CATEGORY: false, // 列表显示文章分类
   POST_LIST_TAG: false, // 列表显示文章标签
 
