c1f22824db84b899980c1e4dc6d3c29d427b585d
MatrixCore <matrixcore@qq.com>
2023-07-25 20:15:32 +0800

    230725

--- themes/medium/config.js
+++ themes/medium/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   POST_LIST_COVER: false, // 文章列表显示图片封面
   POST_LIST_PREVIEW: true, // 列表显示文章预览
-  POST_LIST_CATEGORY: false, // 列表显示文章分类
-  POST_LIST_TAG: false, // 列表显示文章标签
+  POST_LIST_CATEGORY: true, // 列表显示文章分类
+  POST_LIST_TAG: true, // 列表显示文章标签
 
-  POST_DETAIL_CATEGORY: false, // 文章显示分类
-  POST_DETAIL_TAG: false, // 文章显示标签
+  POST_DETAIL_CATEGORY: true, // 文章显示分类
+  POST_DETAIL_TAG: true, // 文章显示标签
 
   // 菜单
-  MENU_CATEGORY: false, // 显示分类
-  MENU_TAG: false, // 显示标签
+  MENU_CATEGORY: true, // 显示分类
+  MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: false, // 显示搜索
+  MENU_SEARCH: true, // 显示搜索
 
   // Widget
   WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
