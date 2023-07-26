63ad6f4fa70f576a57da217cdd8d52ba20faf268
MatrixCore <matrixcore@qq.com>
2023-07-26 15:28:11 +0800

    优化导航栏颜色

--- themes/medium/components/MenuItemDrop.js
+++ themes/medium/components/MenuItemDrop.js
@@ -18,7 +18,7 @@ export const MenuItemDrop = ({ link }) => {
 
         {hasSubMenu &&
             <div className={'px-3 h-full whitespace-nowrap duration-300 text-sm justify-between dark:text-gray-300 cursor-pointer flex flex-nowrap items-center ' +
-                (selected ? 'bg-gray-800 text-white hover:text-white' : 'hover:text-gray-800')}>
+                (selected ? 'bg-orange-300 text-white hover:text-white' : 'hover:text-orange-400')}>
                 <div>
                     {link?.icon && <i className={link?.icon} />} {link?.name}
                     {hasSubMenu && <i className={`px-2 fas fa-chevron-down duration-500 transition-all ${show ? ' rotate-180' : ''}`}></i>}
@@ -28,7 +28,8 @@ export const MenuItemDrop = ({ link }) => {
 
         {!hasSubMenu &&
             <div className={'px-3 h-full whitespace-nowrap duration-300 text-sm justify-between dark:text-gray-300 cursor-pointer flex flex-nowrap items-center ' +
-                (selected ? 'bg-gray-800 text-white hover:text-white' : 'hover:text-gray-500')}>
+                (selected ? 'bg-transparent text-orange-500 hover:text-orange-500' : 'hover:text-orange-400')}>
+                    {/* bg-gray-500是调节选中的背景色的，比如友情链接 */}
                 <Link href={link?.to}>
                     {link?.icon && <i className={link?.icon} />} {link?.name}
                 </Link>
@@ -36,9 +37,10 @@ export const MenuItemDrop = ({ link }) => {
         }
 
         {/* 子菜单 */}
-        {hasSubMenu && <ul className={`${show ? 'visible opacity-100 top-12 ' : 'invisible opacity-0 top-10 '} border-gray-100  bg-white  dark:bg-black dark:border-gray-800 transition-all duration-300 z-20 absolute block drop-shadow-lg `}>
+        {hasSubMenu && <ul className={`${show ? 'visible opacity-100 top-12 ' : 'invisible opacity-0 top-10 '} border-gray-300  bg-orange-100  dark:bg-black dark:border-gray-800 transition-all duration-300 z-20 absolute block drop-shadow-lg `}>
             {link?.subMenus?.map(sLink => {
-              return <li key={sLink.id} className='not:last-child:border-b-0 border-b text-white-500 dark:text-gray-300  hover:bg-gray-300 dark:hover:bg-gray-900 tracking-widest transition-all duration-200  dark:border-gray-800 py-3 pr-6 pl-3'>
+              return <li key={sLink.id} className='not:last-child:border-b-0 border-b text-white-500 dark:text-gray-300  hover:bg-orange-200 dark:hover:bg-gray-900 tracking-widest transition-all duration-200  dark:border-gray-800 py-3 pr-6 pl-3'>
+                    {/* text-white-500是子菜单的字体颜色 */}
                     <Link href={sLink.to}>
                         <span className='text-xs font-extralight'>{link?.icon && <i className={sLink?.icon} > &nbsp; </i>}{sLink.title}</span>
                     </Link>
