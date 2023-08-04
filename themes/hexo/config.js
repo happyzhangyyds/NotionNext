34be9b09574bbe39ef978a20bb00df5440867472
MatrixCore <matrixcore@qq.com>
2023-08-04 23:54:15 +0800

    230805update

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HOME_BANNER_GREETINGS: ['少年辛苦终身事，莫向光阴惰寸功，爱自己是终身浪漫的开始🌹，然后才有勇气选择去爱别人！🎉Welcome to MatrixCore！🎉'], // 首页大图标语文字
 
-  HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
+  HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
   HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
   // 是否显示开始阅读按钮
