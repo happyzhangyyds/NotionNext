b25c39c35dd16dbbd9ef179f6469e492dbeffc40
MatrixCore <matrixcore@qq.com>
2024-02-25 21:38:05 +0800

    微调2

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -24,7 +24,7 @@ export function InfoCard(props) {
             <div className='flex justify-between'>
                 {/* 问候语 */}
                 <GreetingsWords />
-                <div className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-6 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-100 transform transitaion-all duration-200`}>
+                <div className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-6 hover:opacity-0 hover:scale-150 blur-0' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-100 transform transition-all duration-200`}>
                     <LazyImage src={siteInfo?.icon} className='rounded-full' width={isSlugPage ? 100 : 28} alt={siteConfig('AUTHOR')} />
                 </div>
             </div>
