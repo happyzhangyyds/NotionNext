999aedc2c4928a93691df4164e8b889504b3892f
MatrixCore <matrixcore@qq.com>
2024-02-25 21:12:26 +0800

    颜色

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -19,7 +19,7 @@ export function InfoCard(props) {
   // 在文章详情页特殊处理
   const isSlugPage = router.pathname.indexOf('/[prefix]') === 0
   return (
-        <Card className='bg-[#4f65f0] dark:bg-yellow-600 text-white flex flex-col w-72 overflow-hidden relative'>
+        <Card className='bg-[#689377] dark:bg-[#0c2710] text-white flex flex-col w-72 overflow-hidden relative'>
             {/* 信息卡牌第一行 */}
             <div className='flex justify-between'>
                 {/* 问候语 */}
@@ -41,10 +41,10 @@ export function InfoCard(props) {
             <div className='flex justify-between'>
                 <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
                     {/* 两个社交按钮 */}
-                    <div className='bg-indigo-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
+                    <div className='bg-stone-400 p-2 rounded-full  transition-colors duration-200 dark:bg-stone-500 dark:hover:bg-black hover:bg-white'>
                         <Link href='/about'><GlobeAlt className={'w-6 h-6'} /></Link>
                     </div>
-                    <div className='bg-indigo-400 p-2 rounded-full w-10 items-center flex justify-center transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
+                    <div className='bg-stone-400 p-2 rounded-full w-10 items-center flex justify-center transition-colors duration-200 dark:bg-stone-500 dark:hover:bg-black hover:bg-white'>
                         <Link href={siteConfig('HEO_INFO_CARD_URL', null, CONFIG)}><i className='fab fa-github text-xl' />
                         </Link>
                     </div>
@@ -67,7 +67,7 @@ function GreetingsWords() {
     setGreeting(greetings[randomIndex])
   }
 
-  return <div onClick={handleChangeGreeting} className=' select-none cursor-pointer py-1 px-2 bg-indigo-400 hover:bg-indigo-50  hover:text-indigo-950 dark:bg-yellow-500 dark:hover:text-white dark:hover:bg-black text-sm rounded-lg  duration-200 transition-colors'>
+  return <div onClick={handleChangeGreeting} className=' select-none cursor-pointer py-1 px-2 bg-stone-500 hover:bg-indigo-50  hover:text-indigo-950 dark:bg-stone-500 dark:hover:text-white dark:hover:bg-black text-sm rounded-lg  duration-200 transition-colors'>
         {greeting}
     </div>
 }
@@ -78,7 +78,7 @@ function GreetingsWords() {
  */
 function MoreButton() {
   return <Link href='/about'>
-        <div className={'group bg-indigo-400 dark:bg-yellow-500 hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white flex items-center transition-colors duration-200 py-2 px-3 rounded-full space-x-1'}>
+        <div className={'group bg-stone-400 dark:bg-stone-500 hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white flex items-center transition-colors duration-200 py-2 px-3 rounded-full space-x-1'}>
             <ArrowRightCircle className={'group-hover:stroke-black dark:group-hover:stroke-white w-6 h-6 transition-all duration-100'} />
             <div className='font-bold'>了解更多</div>
         </div>
