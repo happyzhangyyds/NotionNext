1092f8901e6ee2b14ff651fe141d42e7b326619b
MatrixCore <matrixcore@qq.com>
2024-03-10 22:43:36 +0800

    footer3

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = ({ title }) => {
             <div id='footer-bottom' className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#30343f]'>
 
                 <div id='footer-bottom-left'>
-                   NotionNext {siteConfig('VERSION')} <i className='fas fa-copyright' /> {`${copyrightDate}`} <i style={{ marginRight: '10px' }} className='mx-1 animate-pulse fas fa-heart text-pink-200' /> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>
+                   NotionNext {siteConfig('VERSION')} <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart text-pink-200' /> <a href={siteConfig('LINK')} style={{ marginRight: '10px' }} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>
                    <a href='http://www.andawell.com' style={{ marginRight: '10px' }} className='underline font-bold  dark:text-gray-300'>Andawell</a>
                    <a href='https://www.wys.me/' style={{ marginRight: '10px' }} className='underline font-bold  dark:text-gray-300'>王语双</a>
                    <a href='https://dusays.com/' style={{ marginRight: '10px' }} className='underline font-bold  dark:text-gray-300'>杜老师</a>
