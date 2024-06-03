ff85ce6168f8ff393e80c72d90b9346099049d8a
MatrixCore <matrixcore@qq.com>
2024-06-03 23:06:08 +0800

    微风起

--- blog.config.js
+++ blog.config.js
@@ -18,10 +18,10 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'MatrixCore', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个喜欢做梦的硬件工程师😋', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'MatrixCore&微风起', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '不是一座孤岛，而是与挚友风雨兼程', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://matrixcore.life', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, MatrixCore,matrixcore,MatrixCore的博客,NotionNext,博客,学习分享,生活体验,Elog', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '微风起，Notion, MatrixCore,matrixcore,MatrixCore的博客,NotionNext,博客,学习分享,生活体验,Elog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'matrixcore@qq.com', // 邮箱地址 例如mail@tangly1024.com
