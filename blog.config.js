e2362f31b28dfba585f33f9c53bf5555249dbbaa
MatrixCore <matrixcore@qq.com>
2024-06-10 19:32:46 +0800

    favicon

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '非常6+1', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '不是一座孤岛，而是与挚友风雨兼程', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://matrixcore.life', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://matrixcore.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, MatrixCore,matrixcore,MatrixCore的博客,NotionNext,博客,学习分享,生活体验,Elog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
@@ -38,7 +38,7 @@ const BLOG = {
 
   NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
 
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://bu.dusays.com/2024/06/10/6666e44cb4d6d.jpeg', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
 
   IMAGE_COMPRESS_WIDTH: process.env.NEXT_PUBLIC_IMAGE_COMPRESS_WIDTH || 500, // 图片压缩宽度默认值，作用于博客封面和文章内容 越小加载图片越快
   IMAGE_ZOOM_IN_WIDTH: process.env.NEXT_PUBLIC_IMAGE_ZOOM_IN_WIDTH || 1200, // 文章图片点击放大后的画质宽度，不代表在网页中的实际展示宽度
